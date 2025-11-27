11fed37b1393004753edb8d777c4745043ce4d1b
laleyechar <sarahlaleye1@gmail.com>
2025-11-28 00:24:45 +0100

    user3

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,8 +15,8 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         // Créer des rôles
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'user']);
-        Role::create(['name' => 'manager']);
+        Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'manager', 'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'user', 'guard_name' => 'web']);
     }
 }
